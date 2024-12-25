46a1e5e60a4fee357df387b73e8e03aea6a2f530
muhammadirfanbaihaqi <muhammadirfanbaihaqi538@gmail.com>
2024-12-25 16:18:47 +0700

    template awal route

--- myapp.py
+++ myapp.py
@@ -19,7 +19,7 @@ def login():
         password = request.form['password']
 
         cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM users WHERE id_pengguna = %s", [id_pengguna]) #MENGAMBIL DATA DARI DATABASE BERDASARKAN ID_PENGGUNA
+        cur.execute("SELECT * FROM msuser WHERE id_petugas = %s", [id_pengguna]) #MENGAMBIL DATA DARI DATABASE BERDASARKAN ID_PENGGUNA
         user = cur.fetchone()
         cur.close()
 
@@ -39,6 +39,65 @@ def login():
 
     return render_template('login.html')
 
+@app.route('/homeOwner', methods=['GET', 'POST'])
+def homeOwner():
+    return render_template('homeOwner.html')
+
+
+
+
+
+@app.route('/checkinOwner', methods=['GET', 'POST'])
+def checkinOwner():
+    return render_template('checkinOwner.html')
+
+
+@app.route('/checkoutOwner', methods=['GET', 'POST'])
+def checkoutOwner():
+    return render_template('checkoutOwner.html')
+
+
+@app.route('/booking_listOwner', methods=['GET', 'POST'])
+def booking_listOwner():
+    return render_template('booking_listOwner.html')
+
+
+@app.route('/room_listOwner', methods=['GET', 'POST'])
+def room_listOwner():
+    return render_template('room_listOwner.html')
+
+
+@app.route('/managePetugas', methods=['GET', 'POST'])
+def managePetugas():
+    return render_template('managePetugas.html')
+
+
+
+@app.route('/homePetugas', methods=['GET', 'POST'])
+def homePetugas():
+    return render_template('homePetugas.html')
+
+@app.route('/checkin', methods=['GET', 'POST'])
+def checkin():
+    return render_template('checkin.html')
+
+
+@app.route('/checkout', methods=['GET', 'POST'])
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/booking_list', methods=['GET', 'POST'])
+def booking_list():
+    return render_template('booking_list.html')
+
+
+@app.route('/room_list', methods=['GET', 'POST'])
+def room_list():
+    return render_template('room_list.html')
+
+
+
+
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
@@ -46,7 +105,7 @@ def logout():
     session.pop('name', None)
     #The None argument ensures that no error is raised if the key doesn't exist. 
     flash('You have been logged out.', 'success')
-    return redirect(url_for('login'))
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
