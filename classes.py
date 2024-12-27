814ccc286d5d7f265fbe522ee5581677e2138703
muhammadirfanbaihaqi <muhammadirfanbaihaqi538@gmail.com>
2024-12-27 20:58:20 +0700

    before pagination

--- classes.py
+++ classes.py
@@ -89,3 +89,260 @@ class Struk:
 
 
     
+class Owner:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def checkin(cls, nik, lantai_ke, kategori, nama_pelanggan, durasi, id_petugas, waktu_checkin, id_booking):
+        try:
+            conn = mysql.connect
+            cursor = conn.cursor()
+            # Select kamar sesuai kriteria
+            cursor.execute(
+                """
+                SELECT * FROM mskamar 
+                WHERE lantai_ke = %s AND kategori = %s AND statuskamar = 'Tersedia'
+                LIMIT 1
+                """,
+                (lantai_ke, kategori)
+            )
+            kamar = cursor.fetchone()
+
+            if not kamar:
+                flash("Tidak ada kamar yang tersedia sesuai kriteria.", "danger")
+                return redirect(url_for('checkinOwner'))
+
+            kode_kamar = kamar[0]
+            harga_kamar = kamar[2]
+            harga_bayar_awal = durasi * harga_kamar
+
+            # Update status kamar
+            cursor.execute(
+                """
+                UPDATE mskamar 
+                SET statuskamar = 'Dibooking' 
+                WHERE Kode_Kamar = %s
+                """,
+                (kode_kamar,)
+            )
+            conn.commit()
+
+            # Insert booking data
+            cursor.execute(
+                """
+                INSERT INTO trbooking (ID_Booking, NIK, nama_pelanggan ,ID_Petugas, Kode_Kamar, Waktu_Checkin, Durasi_Hari, HargaBayarAwal) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                    """,
+                (id_booking, nik, nama_pelanggan ,id_petugas, kode_kamar, waktu_checkin, durasi, harga_bayar_awal)
+            )
+            conn.commit()
+            flash("Booking berhasil!", "success")
+
+        except Exception as e:
+            conn.rollback()
+            flash(f"Terjadi kesalahan: {str(e)}", "danger")
+        finally:
+            conn.close()
+
+    @classmethod
+    def lihatBooking(cls, cursor, ):
+        nik = request.args.get('nik', '').strip()
+        tanggal = request.args.get('tanggal', '').strip()
+
+        booking_list_items = []
+
+        # Query dasar
+        query = """
+            SELECT 
+                tr.ID_Booking, tr.NIK, tr.nama_pelanggan, tr.ID_Petugas, 
+                tr.Kode_Kamar, tr.Waktu_Checkin, tr.Durasi_Hari, 
+                tr.waktu_checkout, tr.HargaBayarAwal, tr.denda, tr.hargafinal 
+            FROM trbooking tr
+            WHERE 1=1
+        """
+        params = []
+
+        # Tambahkan filter berdasarkan NIK jika diisi
+        if nik:
+            query += " AND tr.NIK = %s"
+            params.append(nik)
+
+        # Tambahkan filter berdasarkan tanggal jika diisi
+        if tanggal:
+            query += " AND DATE(tr.Waktu_Checkin) = %s"
+            params.append(tanggal)
+
+        cursor.execute(query, tuple(params))
+        bookings = cursor.fetchall()
+
+        # Konversi data dari database ke dalam dictionary
+        booking_list_items = [
+            {
+                'id_booking': booking[0],
+                'nik': booking[1],
+                'nama_pelanggan': booking[2],
+                'id_petugas': booking[3],
+                'kode_kamar': booking[4],
+                'waktu_checkin': booking[5],
+                'durasi_hari': booking[6],
+                'waktu_checkout': booking[7],
+                'harga_bayar_awal': booking[8],
+                'denda': booking[9],
+                'harga_final': booking[10],
+            }
+            for booking in bookings
+        ]
+
+        return booking_list_items
+    
+
+    @classmethod
+    def checkout(cls, conn ,cursor):
+        booking_id = request.form['booking_id']
+        current_time = datetime.now()
+
+        cursor.execute("""
+            SELECT Waktu_Checkin, Durasi_Hari, HargaBayarAwal, Kode_Kamar
+            FROM trbooking 
+            WHERE ID_Booking = %s
+        """, (booking_id,))
+        booking = cursor.fetchone()
+
+        if not booking:
+            flash('Booking tidak ditemukan.', 'danger')
+            return redirect(url_for('booking_listOwner'))
+
+        waktu_checkin = booking[0]
+        durasi_hari = booking[1]
+        harga_bayar_awal = booking[2]
+        kode_kamar = booking[3]
+
+        # Calculate Jadwal_CO_Seharusnya
+        jadwal_co_seharusnya = (waktu_checkin + timedelta(days=durasi_hari)).replace(
+            hour=12, minute=0, second=0, microsecond=0
+        )
+
+        if current_time > jadwal_co_seharusnya:
+            waktu_terlewat = math.ceil((current_time - jadwal_co_seharusnya).total_seconds() / 3600)
+            denda = 0.05 * harga_bayar_awal * waktu_terlewat
+            harga_final = harga_bayar_awal + denda
+        else:
+            denda = 0
+            harga_final = harga_bayar_awal
+
+        cursor.execute("""
+            UPDATE trbooking 
+            SET waktu_checkout = %s, denda = %s, hargafinal = %s 
+            WHERE ID_Booking = %s
+        """, (current_time, denda, harga_final, booking_id))
+        conn.commit()
+
+        cursor.execute("""
+            UPDATE mskamar
+            SET statuskamar = 'Tersedia'
+            WHERE Kode_Kamar = %s
+        """, (kode_kamar,))
+        conn.commit()
+
+        flash('Checkout berhasil!', 'success')
+        return redirect(url_for('booking_listOwner'))
+    
+    @classmethod
+    def melihat_daftar_kamar(cls):
+        per_page = 9
+        page = int(request.args.get('page', 1))
+
+        cur = mysql.connection.cursor()
+
+        # Hitung jumlah total kamar
+        cur.execute("SELECT COUNT(*) FROM mskamar")
+        total_rooms = cur.fetchone()[0]
+
+        total_pages = math.ceil(total_rooms / per_page)
+        offset = (page - 1) * per_page
+
+        # Ambil data kamar dengan pagination
+        cur.execute(
+            "SELECT Kode_Kamar, foto, Kategori, harga_kamar, statuskamar FROM mskamar LIMIT %s OFFSET %s",
+            (per_page, offset)
+        )
+        rooms = cur.fetchall()
+        cur.close()
+
+        room_list = [
+            {
+                'id_kamar': r[0],
+                'foto': 'data:image/jpeg;base64,' + base64.b64encode(r[1]).decode('utf-8'),  # Konversi byte ke base64
+                'tipe_kamar': r[2],
+                'harga_kamar': r[3],
+                'statuskamar': r[4],
+            }
+            for r in rooms
+        ]
+
+    
+
+        return render_template('room_listOwner.html', rooms=room_list, total_pages=total_pages, current_page=page)
+    
+    @classmethod
+    def delete_room(cls,id):
+        try:
+            # Koneksi ke database
+            conn = mysql.connection
+            cur = conn.cursor()
+
+            # Hapus kamar berdasarkan id
+            cur.execute("DELETE FROM mskamar WHERE Kode_Kamar = %s", (id,))
+            conn.commit()
+            cur.close()
+            conn.close()
+            return '', 200
+        except Exception as e:
+            print(e)
+            return 'Failed to delete room.', 500
+        
+    @classmethod
+    def add_room(cls):
+        try:
+            kode_kamar = request.form['kode_kamar']
+            kategori = request.form['kategori']
+            harga_kamar = request.form['harga_kamar']
+            lantai_ke = request.form['lantai_ke']
+            foto = request.files['foto']
+
+            if foto:
+                filename = secure_filename(foto.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                foto.save(filepath)
+
+            # Baca file gambar sebagai byte
+                with open(filepath, 'rb') as file:
+                    byte_data = file.read()
+
+            conn = mysql.connection
+            cursor = conn.cursor()
+
+            # Simpan data byte ke database
+            cursor.execute(
+                "INSERT INTO mskamar (Kode_Kamar, Kategori, harga_kamar, foto, lantai_ke) VALUES (%s, %s, %s, %s, %s)",
+                (kode_kamar, kategori, harga_kamar, byte_data, lantai_ke)
+            )
+            conn.commit()
+
+            cursor.close()
+            conn.close()
+
+            flash('Berhasil menambahkan kamar!', 'success')
+            response = redirect(url_for('room_listOwner'))
+            response.headers['X-Success'] = 'true'  # Tambahkan header kustom
+            return response
+        except Exception as e:
+            print(e)
+            flash('Gagal menambahkan kamar.', 'danger')
+            response = redirect(url_for('room_listOwner'))
+            response.headers['X-Success'] = 'false'  # Tambahkan header kustom
+            return response
+
+        
+
--- myapp.py
+++ myapp.py
@@ -13,7 +13,7 @@ from reportlab.pdfgen import canvas
 from io import BytesIO
 
 # import class
-from classes import Struk
+from classes import Struk, Owner
 
 
 app = Flask(__name__)
@@ -76,55 +76,57 @@ def checkinOwner():
         waktu_checkin = datetime.now()
         id_booking = random.randint(1000, 9999)
 
-        try:
-            conn = mysql.connect
-            cursor = conn.cursor()
-            # Select kamar sesuai kriteria
-            cursor.execute(
-                """
-                SELECT * FROM mskamar 
-                WHERE lantai_ke = %s AND kategori = %s AND statuskamar = 'Tersedia'
-                LIMIT 1
-                """,
-                (lantai_ke, kategori)
-            )
-            kamar = cursor.fetchone()
+        Owner.checkin(nik, lantai_ke, kategori, nama_pelanggan, durasi, id_petugas, waktu_checkin, id_booking)
 
-            if not kamar:
-                flash("Tidak ada kamar yang tersedia sesuai kriteria.", "danger")
-                return redirect(url_for('checkinOwner'))
+        # try:
+        #     conn = mysql.connect
+        #     cursor = conn.cursor()
+        #     # Select kamar sesuai kriteria
+        #     cursor.execute(
+        #         """
+        #         SELECT * FROM mskamar 
+        #         WHERE lantai_ke = %s AND kategori = %s AND statuskamar = 'Tersedia'
+        #         LIMIT 1
+        #         """,
+        #         (lantai_ke, kategori)
+        #     )
+        #     kamar = cursor.fetchone()
 
-            kode_kamar = kamar[0]
-            harga_kamar = kamar[2]
-            harga_bayar_awal = durasi * harga_kamar
+        #     if not kamar:
+        #         flash("Tidak ada kamar yang tersedia sesuai kriteria.", "danger")
+        #         return redirect(url_for('checkinOwner'))
 
-            # Update status kamar
-            cursor.execute(
-                """
-                UPDATE mskamar 
-                SET statuskamar = 'Dibooking' 
-                WHERE Kode_Kamar = %s
-                """,
-                (kode_kamar,)
-            )
-            conn.commit()
+        #     kode_kamar = kamar[0]
+        #     harga_kamar = kamar[2]
+        #     harga_bayar_awal = durasi * harga_kamar
 
-            # Insert booking data
-            cursor.execute(
-                """
-                INSERT INTO trbooking (ID_Booking, NIK, nama_pelanggan ,ID_Petugas, Kode_Kamar, Waktu_Checkin, Durasi_Hari, HargaBayarAwal) 
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                    """,
-                (id_booking, nik, nama_pelanggan ,id_petugas, kode_kamar, waktu_checkin, durasi, harga_bayar_awal)
-            )
-            conn.commit()
-            flash("Booking berhasil!", "success")
+        #     # Update status kamar
+        #     cursor.execute(
+        #         """
+        #         UPDATE mskamar 
+        #         SET statuskamar = 'Dibooking' 
+        #         WHERE Kode_Kamar = %s
+        #         """,
+        #         (kode_kamar,)
+        #     )
+        #     conn.commit()
 
-        except Exception as e:
-            conn.rollback()
-            flash(f"Terjadi kesalahan: {str(e)}", "danger")
-        finally:
-            conn.close()
+        #     # Insert booking data
+        #     cursor.execute(
+        #         """
+        #         INSERT INTO trbooking (ID_Booking, NIK, nama_pelanggan ,ID_Petugas, Kode_Kamar, Waktu_Checkin, Durasi_Hari, HargaBayarAwal) 
+        #         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        #             """,
+        #         (id_booking, nik, nama_pelanggan ,id_petugas, kode_kamar, waktu_checkin, durasi, harga_bayar_awal)
+        #     )
+        #     conn.commit()
+        #     flash("Booking berhasil!", "success")
+
+        # except Exception as e:
+        #     conn.rollback()
+        #     flash(f"Terjadi kesalahan: {str(e)}", "danger")
+        # finally:
+        #     conn.close()
 
         return redirect(url_for('checkinOwner'))
 
@@ -141,104 +143,113 @@ def booking_listOwner():
     conn = mysql.connect
     cursor = conn.cursor()
 
+    # Mengambil data booking
+    booking_list_items = Owner.lihatBooking(cursor)
+
+
     # Inisialisasi variabel untuk menampung data pencarian
-    nik = request.args.get('nik', '').strip()
-    tanggal = request.args.get('tanggal', '').strip()
+    # nik = request.args.get('nik', '').strip()
+    # tanggal = request.args.get('tanggal', '').strip()
 
-    booking_list_items = []
+    # booking_list_items = []
 
-    # Query dasar
-    query = """
-        SELECT 
-            tr.ID_Booking, tr.NIK, tr.nama_pelanggan, tr.ID_Petugas, 
-            tr.Kode_Kamar, tr.Waktu_Checkin, tr.Durasi_Hari, 
-            tr.waktu_checkout, tr.HargaBayarAwal, tr.denda, tr.hargafinal 
-        FROM trbooking tr
-        WHERE 1=1
-    """
-    params = []
+    # # Query dasar
+    # query = """
+    #     SELECT 
+    #         tr.ID_Booking, tr.NIK, tr.nama_pelanggan, tr.ID_Petugas, 
+    #         tr.Kode_Kamar, tr.Waktu_Checkin, tr.Durasi_Hari, 
+    #         tr.waktu_checkout, tr.HargaBayarAwal, tr.denda, tr.hargafinal 
+    #     FROM trbooking tr
+    #     WHERE 1=1
+    # """
+    # params = []
 
-    # Tambahkan filter berdasarkan NIK jika diisi
-    if nik:
-        query += " AND tr.NIK = %s"
-        params.append(nik)
+    # # Tambahkan filter berdasarkan NIK jika diisi
+    # if nik:
+    #     query += " AND tr.NIK = %s"
+    #     params.append(nik)
 
-    # Tambahkan filter berdasarkan tanggal jika diisi
-    if tanggal:
-        query += " AND DATE(tr.Waktu_Checkin) = %s"
-        params.append(tanggal)
+    # # Tambahkan filter berdasarkan tanggal jika diisi
+    # if tanggal:
+    #     query += " AND DATE(tr.Waktu_Checkin) = %s"
+    #     params.append(tanggal)
 
-    cursor.execute(query, tuple(params))
-    bookings = cursor.fetchall()
+    # cursor.execute(query, tuple(params))
+    # bookings = cursor.fetchall()
 
-    # Konversi data dari database ke dalam dictionary
-    booking_list_items = [
-        {
-            'id_booking': booking[0],
-            'nik': booking[1],
-            'nama_pelanggan': booking[2],
-            'id_petugas': booking[3],
-            'kode_kamar': booking[4],
-            'waktu_checkin': booking[5],
-            'durasi_hari': booking[6],
-            'waktu_checkout': booking[7],
-            'harga_bayar_awal': booking[8],
-            'denda': booking[9],
-            'harga_final': booking[10],
-        }
-        for booking in bookings
-    ]
+    # # Konversi data dari database ke dalam dictionary
+    # booking_list_items = [
+    #     {
+    #         'id_booking': booking[0],
+    #         'nik': booking[1],
+    #         'nama_pelanggan': booking[2],
+    #         'id_petugas': booking[3],
+    #         'kode_kamar': booking[4],
+    #         'waktu_checkin': booking[5],
+    #         'durasi_hari': booking[6],
+    #         'waktu_checkout': booking[7],
+    #         'harga_bayar_awal': booking[8],
+    #         'denda': booking[9],
+    #         'harga_final': booking[10],
+    #     }
+    #     for booking in bookings
+    # ]
 
     # Bagian POST tetap sama seperti sebelumnya
     if request.method == 'POST':
-        booking_id = request.form['booking_id']
-        current_time = datetime.now()
+        Owner.checkout(conn, cursor)
 
-        cursor.execute("""
-            SELECT Waktu_Checkin, Durasi_Hari, HargaBayarAwal, Kode_Kamar
-            FROM trbooking 
-            WHERE ID_Booking = %s
-        """, (booking_id,))
-        booking = cursor.fetchone()
+        # Mengambil data booking setelah checkout
+        booking_list_items = Owner.lihatBooking(cursor)
 
-        if not booking:
-            flash('Booking tidak ditemukan.', 'danger')
-            return redirect(url_for('booking_listOwner'))
+        # booking_id = request.form['booking_id']
+        # current_time = datetime.now()
 
-        waktu_checkin = booking[0]
-        durasi_hari = booking[1]
-        harga_bayar_awal = booking[2]
-        kode_kamar = booking[3]
+        # cursor.execute("""
+        #     SELECT Waktu_Checkin, Durasi_Hari, HargaBayarAwal, Kode_Kamar
+        #     FROM trbooking 
+        #     WHERE ID_Booking = %s
+        # """, (booking_id,))
+        # booking = cursor.fetchone()
 
-        # Calculate Jadwal_CO_Seharusnya
-        jadwal_co_seharusnya = (waktu_checkin + timedelta(days=durasi_hari)).replace(
-            hour=12, minute=0, second=0, microsecond=0
-        )
+        # if not booking:
+        #     flash('Booking tidak ditemukan.', 'danger')
+        #     return redirect(url_for('booking_listOwner'))
 
-        if current_time > jadwal_co_seharusnya:
-            waktu_terlewat = math.ceil((current_time - jadwal_co_seharusnya).total_seconds() / 3600)
-            denda = 0.05 * harga_bayar_awal * waktu_terlewat
-            harga_final = harga_bayar_awal + denda
-        else:
-            denda = 0
-            harga_final = harga_bayar_awal
+        # waktu_checkin = booking[0]
+        # durasi_hari = booking[1]
+        # harga_bayar_awal = booking[2]
+        # kode_kamar = booking[3]
 
-        cursor.execute("""
-            UPDATE trbooking 
-            SET waktu_checkout = %s, denda = %s, hargafinal = %s 
-            WHERE ID_Booking = %s
-        """, (current_time, denda, harga_final, booking_id))
-        conn.commit()
+        # # Calculate Jadwal_CO_Seharusnya
+        # jadwal_co_seharusnya = (waktu_checkin + timedelta(days=durasi_hari)).replace(
+        #     hour=12, minute=0, second=0, microsecond=0
+        # )
 
-        cursor.execute("""
-            UPDATE mskamar
-            SET statuskamar = 'Tersedia'
-            WHERE Kode_Kamar = %s
-        """, (kode_kamar,))
-        conn.commit()
+        # if current_time > jadwal_co_seharusnya:
+        #     waktu_terlewat = math.ceil((current_time - jadwal_co_seharusnya).total_seconds() / 3600)
+        #     denda = 0.05 * harga_bayar_awal * waktu_terlewat
+        #     harga_final = harga_bayar_awal + denda
+        # else:
+        #     denda = 0
+        #     harga_final = harga_bayar_awal
 
-        flash('Checkout berhasil!', 'success')
-        return redirect(url_for('booking_listOwner'))
+        # cursor.execute("""
+        #     UPDATE trbooking 
+        #     SET waktu_checkout = %s, denda = %s, hargafinal = %s 
+        #     WHERE ID_Booking = %s
+        # """, (current_time, denda, harga_final, booking_id))
+        # conn.commit()
+
+        # cursor.execute("""
+        #     UPDATE mskamar
+        #     SET statuskamar = 'Tersedia'
+        #     WHERE Kode_Kamar = %s
+        # """, (kode_kamar,))
+        # conn.commit()
+
+        # flash('Checkout berhasil!', 'success')
+        # return redirect(url_for('booking_listOwner'))
 
     cursor.close()
     conn.close()
@@ -264,101 +275,108 @@ def room_listOwner():
     if role == "Petugas":
         flash("Anda tidak memiliki akses ke halaman ini. Anda telah logout.", "danger")
         return redirect(url_for('logout'))
-    per_page = 9
-    page = int(request.args.get('page', 1))
+    
 
-    cur = mysql.connection.cursor()
+    return Owner.melihat_daftar_kamar()
 
-    # Hitung jumlah total kamar
-    cur.execute("SELECT COUNT(*) FROM mskamar")
-    total_rooms = cur.fetchone()[0]
+    # per_page = 9
+    # page = int(request.args.get('page', 1))
 
-    total_pages = math.ceil(total_rooms / per_page)
-    offset = (page - 1) * per_page
+    # cur = mysql.connection.cursor()
 
-    # Ambil data kamar dengan pagination
-    cur.execute(
-        "SELECT Kode_Kamar, foto, Kategori, harga_kamar, statuskamar FROM mskamar LIMIT %s OFFSET %s",
-        (per_page, offset)
-    )
-    rooms = cur.fetchall()
-    cur.close()
+    # # Hitung jumlah total kamar
+    # cur.execute("SELECT COUNT(*) FROM mskamar")
+    # total_rooms = cur.fetchone()[0]
 
-    room_list = [
-        {
-            'id_kamar': r[0],
-            'foto': 'data:image/jpeg;base64,' + base64.b64encode(r[1]).decode('utf-8'),  # Konversi byte ke base64
-            'tipe_kamar': r[2],
-            'harga_kamar': r[3],
-            'statuskamar': r[4],
-        }
-        for r in rooms
-    ]
+    # total_pages = math.ceil(total_rooms / per_page)
+    # offset = (page - 1) * per_page
 
-    return render_template('room_listOwner.html', rooms=room_list, total_pages=total_pages, current_page=page)
+    # # Ambil data kamar dengan pagination
+    # cur.execute(
+    #     "SELECT Kode_Kamar, foto, Kategori, harga_kamar, statuskamar FROM mskamar LIMIT %s OFFSET %s",
+    #     (per_page, offset)
+    # )
+    # rooms = cur.fetchall()
+    # cur.close()
+
+    # room_list = [
+    #     {
+    #         'id_kamar': r[0],
+    #         'foto': 'data:image/jpeg;base64,' + base64.b64encode(r[1]).decode('utf-8'),  # Konversi byte ke base64
+    #         'tipe_kamar': r[2],
+    #         'harga_kamar': r[3],
+    #         'statuskamar': r[4],
+    #     }
+    #     for r in rooms
+    # ]
+
+    # return render_template('room_listOwner.html', rooms=room_list, total_pages=total_pages, current_page=page)
 
 
 # Route untuk menghapus kamar
 @app.route('/delete-room/<id>', methods=['POST'])
 def delete_room(id):
-    try:
-        # Koneksi ke database
-        conn = mysql.connection
-        cur = conn.cursor()
+    return Owner.delete_room(id)
+    # try:
+    #     # Koneksi ke database
+    #     conn = mysql.connection
+    #     cur = conn.cursor()
 
-        # Hapus kamar berdasarkan id
-        cur.execute("DELETE FROM mskamar WHERE Kode_Kamar = %s", (id,))
-        conn.commit()
-        cur.close()
-        conn.close()
-        return '', 200
-    except Exception as e:
-        print(e)
-        return 'Failed to delete room.', 500
+    #     # Hapus kamar berdasarkan id
+    #     cur.execute("DELETE FROM mskamar WHERE Kode_Kamar = %s", (id,))
+    #     conn.commit()
+    #     cur.close()
+    #     conn.close()
+    #     return '', 200
+    # except Exception as e:
+    #     print(e)
+    #     return 'Failed to delete room.', 500
 
 
 
 @app.route('/add-room', methods=['POST'])
 def add_room():
-    try:
-        kode_kamar = request.form['kode_kamar']
-        kategori = request.form['kategori']
-        harga_kamar = request.form['harga_kamar']
-        lantai_ke = request.form['lantai_ke']
-        foto = request.files['foto']
+    return Owner.add_room()
 
-        if foto:
-            filename = secure_filename(foto.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            foto.save(filepath)
+    # try:
+    #     kode_kamar = request.form['kode_kamar']
+    #     kategori = request.form['kategori']
+    #     harga_kamar = request.form['harga_kamar']
+    #     lantai_ke = request.form['lantai_ke']
+    #     foto = request.files['foto']
 
-            # Baca file gambar sebagai byte
-            with open(filepath, 'rb') as file:
-                byte_data = file.read()
+    #     if foto:
+    #         filename = secure_filename(foto.filename)
+    #         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    #         foto.save(filepath)
 
-        conn = mysql.connection
-        cursor = conn.cursor()
+    #         # Baca file gambar sebagai byte
+    #         with open(filepath, 'rb') as file:
+    #             byte_data = file.read()
 
-        # Simpan data byte ke database
-        cursor.execute(
-            "INSERT INTO mskamar (Kode_Kamar, Kategori, harga_kamar, foto, lantai_ke) VALUES (%s, %s, %s, %s, %s)",
-            (kode_kamar, kategori, harga_kamar, byte_data, lantai_ke)
-        )
-        conn.commit()
+    #     conn = mysql.connection
+    #     cursor = conn.cursor()
 
-        cursor.close()
-        conn.close()
+    #     # Simpan data byte ke database
+    #     cursor.execute(
+    #         "INSERT INTO mskamar (Kode_Kamar, Kategori, harga_kamar, foto, lantai_ke) VALUES (%s, %s, %s, %s, %s)",
+    #         (kode_kamar, kategori, harga_kamar, byte_data, lantai_ke)
+    #     )
+    #     conn.commit()
 
-        flash('Berhasil menambahkan kamar!', 'success')
-        response = redirect(url_for('room_listOwner'))
-        response.headers['X-Success'] = 'true'  # Tambahkan header kustom
-        return response
-    except Exception as e:
-        print(e)
-        flash('Gagal menambahkan kamar.', 'danger')
-        response = redirect(url_for('room_listOwner'))
-        response.headers['X-Success'] = 'false'  # Tambahkan header kustom
-        return response
+    #     cursor.close()
+    #     conn.close()
+
+    #     flash('Berhasil menambahkan kamar!', 'success')
+    #     response = redirect(url_for('room_listOwner'))
+    #     response.headers['X-Success'] = 'true'  # Tambahkan header kustom
+    #     return response
+    # except Exception as e:
+    #     print(e)
+    #     flash('Gagal menambahkan kamar.', 'danger')
+    #     response = redirect(url_for('room_listOwner'))
+    #     response.headers['X-Success'] = 'false'  # Tambahkan header kustom
+    #     return response
 
 
 
